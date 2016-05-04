cd21d8d775a5f1af99205c2b377c7a96fe644f17
Eduardo RC <eduardo.rodriguezcarro@servexternos.isban.es>
2016-05-04 15:39:22 +0200

    Main Window

--- src/main/java/org/erc/xlstowiki/MainWindow.java
+++ src/main/java/org/erc/xlstowiki/MainWindow.java
@@ -3,10 +3,19 @@ package org.erc.xlstowiki;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JComboBox;
+import javax.swing.JTextPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MainWindow {
 
 	private JFrame frame;
+	private JTextField txtFile;
 
 	/**
 	 * Launch the application.
@@ -36,8 +45,55 @@ public class MainWindow {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.setResizable(false);
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblFile = new JLabel("File");
+		lblFile.setBounds(20, 11, 79, 14);
+		frame.getContentPane().add(lblFile);
+		
+		txtFile = new JTextField();
+		txtFile.setEditable(false);
+		txtFile.setBounds(109, 8, 230, 20);
+		frame.getContentPane().add(txtFile);
+		txtFile.setColumns(10);
+		
+		JButton btnLoad = new JButton("Load");
+		btnLoad.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//TODO
+			}
+		});
+		btnLoad.setBounds(345, 7, 89, 23);
+		frame.getContentPane().add(btnLoad);
+		
+		JPanel pnlOptions = new JPanel();
+		pnlOptions.setBounds(10, 36, 424, 225);
+		frame.getContentPane().add(pnlOptions);
+		pnlOptions.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Sheet");
+		lblNewLabel.setBounds(10, 11, 78, 14);
+		pnlOptions.add(lblNewLabel);
+		
+		JComboBox cmbSheet = new JComboBox();
+		cmbSheet.setBounds(98, 8, 226, 20);
+		pnlOptions.add(cmbSheet);
+		
+		JButton btnConvert = new JButton("Convert");
+		btnConvert.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//TODO
+			}
+		});
+		btnConvert.setBounds(334, 7, 89, 23);
+		pnlOptions.add(btnConvert);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setEditable(false);
+		textPane.setBounds(10, 36, 413, 178);
+		pnlOptions.add(textPane);
 	}
-
 }
